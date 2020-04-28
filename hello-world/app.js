18e036257ff5b9049e76558054716c89c8755b0f
Dieguin <diegocarrillo@gmail.com>
2020-04-28 17:04:16 -0500

    Updating app

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hola mundooooooo',
+                message: 'adios mundo cruel',
                 // location: ret.data.trim()
             })
         }
