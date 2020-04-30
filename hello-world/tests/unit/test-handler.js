9871aaf6c746dfa0d2daa4b7030007b7964621a7
Diego <diegocarrillo.cr@gmail.com>
2020-04-30 16:02:38 -0500

    Update message

--- hello-world/tests/unit/test-handler.js
+++ hello-world/tests/unit/test-handler.js
@@ -16,7 +16,7 @@ describe('Tests index', function () {
         let response = JSON.parse(result.body);
 
         expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("adios mundo cruel");
+        expect(response.message).to.be.equal("hola a todos");
         // expect(response.location).to.be.an("string");
     });
 });
